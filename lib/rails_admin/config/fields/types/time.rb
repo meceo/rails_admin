2484be74fefbc24b5e63bec07dce6b084152ef7b
Mateusz Juraszek <mateusz@webtigers.eu>
2014-02-19 11:17:35 +0100

    remove parse_date_string override

--- lib/rails_admin/config/fields/types/time.rb
+++ lib/rails_admin/config/fields/types/time.rb
@@ -21,11 +21,6 @@ module RailsAdmin
             params[name] = self.class.normalize(params[name], localized_time_format) if params[name].present?
           end
 
-          # Parse normalized date (time) strings using UTC
-          def self.parse_date_string(date_string)
-            ::DateTime.parse(date_string)
-          end
-
           register_instance_option :strftime_format do
             (localized_format.include? '%p') ? '%I:%M %p' : '%H:%M'
           end
